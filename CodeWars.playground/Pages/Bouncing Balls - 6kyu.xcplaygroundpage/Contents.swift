77bf2160ca6beab8fe50135998ebb0b30f97b752
Kamil Wrobel <wrobelekpl@hotmail.com>
2022-01-31 20:27:48 -0500

    add other clever solutions

--- CodeWars.playground/Pages/Bouncing Balls - 6kyu.xcplaygroundpage/Contents.swift
+++ CodeWars.playground/Pages/Bouncing Balls - 6kyu.xcplaygroundpage/Contents.swift
@@ -27,22 +27,29 @@ import Foundation
 
 
 func bouncingBall(_ h: Double, _ bounce: Double, _ window: Double) -> Int {
-  
+
   guard h > 0 && bounce > 0 && bounce < 1 && window < h else { return -1}
-  
+
   var count = 0
   var currentHeight = h
-  
+
   while currentHeight > window {
     currentHeight = currentHeight * bounce
     count += 2
   }
-  
+
   return count - 1
 }
 
 
+//other solutions:
 
+//func bouncingBall(_ h: Double, _ bounce: Double, _ window: Double) -> Int {
+//    if !(h > 0 && 0 < bounce && bounce < 1 && window < h) {
+//        return -1
+//    }
+//    return Int(ceil(log(window / h) / log(bounce))) * 2 - 1
+//}
 
 
 
@@ -58,9 +65,9 @@ class BouncingBallsTests : XCTestCase {
     
     func testExample() {
         testequal(3, 0.66, 1.5, 3)
-        testequal(30, 0.66, 1.5, 15)
-        testequal(10, 0.6, 10, -1)
-        testequal(40, 1, 10, -1)
+//        testequal(30, 0.66, 1.5, 15)
+//        testequal(10, 0.6, 10, -1)
+//        testequal(40, 1, 10, -1)
     }
     
 }
